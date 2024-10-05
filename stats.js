844ceddf2629cba5150bf1ed9ee023b3512aa9da
Glaze96 <rasmusramen@gmail.com>
2024-10-05 17:23:46 +0200

    LD56-009: Improve assignment structure

--- stats.js
+++ stats.js
@@ -1,4 +1,32 @@
 const activeAssignments = []
+function checkAssignments() {
+  console.log("test");
+}
 
 var mouthAmount = 0;
 var foodAmount = 0;
+
+let data = {
+  mouthAmount: 0,
+  foodAmount: 0
+};
+
+Object.defineProperty(data, "mouthAmount", {
+  get() {
+      return mouthAmount;
+  },
+  set(value) {
+      mouthAmount = value;
+      checkAssignments();
+  }
+});
+
+Object.defineProperty(data, "foodAmount", {
+  get() {
+      return foodAmount;
+  },
+  set(value) {
+      foodAmount = value;
+      checkAssignments();
+  }
+});
