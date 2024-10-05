6836874fa53df51f903f4dc74a986a70fc970de2
Glaze96 <rasmusramen@gmail.com>
2024-10-05 19:59:44 +0200

    added 2 emails, immunity and defence

--- stats.js
+++ stats.js
@@ -23,12 +23,18 @@ var mouthAmount = 0;
 var foodAmount = 0;
 var warmAmount = 0;
 var coldAmount = 0;
+var virusAmount = 0;
+var radioactivityAmount = 0;
+var killAmount = 0;
 
 let data = {
 	mouthAmount: 0,
 	foodAmount: 0,
 	warmAmount: 0,
 	coldAmount: 0,
+	virusAmount: 0,
+	radioactivityAmount: 0,
+	killAmount: 0,
 };
 
 Object.defineProperty(data, "mouthAmount", {
@@ -69,5 +75,35 @@ Object.defineProperty(data, "coldAmount", {
 	set(value) {
 		coldAmount = value;
 		checkAssignments(coldAmount);
+	},
+});
+
+Object.defineProperty(data, "virusAmount", {
+	get() {
+		return virusAmount;
+	},
+	set(value) {
+		virusAmount = value;
+		checkAssignments(virusAmount);
+	},
+});
+
+Object.defineProperty(data, "radioactivityAmount", {
+	get() {
+		return radioactivityAmount;
+	},
+	set(value) {
+		radioactivityAmount = value;
+		checkAssignments(radioactivityAmount);
+	},
+});
+
+Object.defineProperty(data, "killAmount", {
+	get() {
+		return killAmount;
+	},
+	set(value) {
+		killAmount = value;
+		checkAssignments(killAmount);
 	},
 });
